4644c59157b4ee374f2f8565aff66c9c6cc2ae9f
Dom Christie <christiedom@gmail.com>
2016-04-14 10:43:14 +0100

    Make launch screen red for demo

--- Example/Example.js
+++ Example/Example.js
@@ -54,7 +54,7 @@ export default class Example extends React.Component {
                     <Scene key="register" component={Register} title="Register"/>
                     <Scene key="register2" component={Register} title="Register2" duration={1}/>
                     <Scene key="home" component={Home} title="Replace" type="replace"/>
-                    <Scene key="launch" component={Launch} title="Launch" initial={true} />
+                    <Scene key="launch" component={Launch} title="Launch" initial={true} sceneStyle={{backgroundColor: 'red'}} />
                     <Scene key="login" direction="vertical"  >
                         <Scene key="loginModal" component={Login} title="Login"/>
                         <Scene key="loginModal2" hideNavBar={true} component={Login2} title="Login2" panHandlers={null} duration={1}/>
